ac04bc5668a3e959dc53f12539bf69ba265cd126
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-14 20:25:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var config = {
     title: 'Kanal İstanbul Projesindeki İmar Faaliyetleri / Urban Expansion in the Canal Istanbul Project',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     chapters: [
         {
             id: '1',
